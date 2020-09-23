5c4f2c9f8df078f45feb680a725b36f7e69cd5a0
chai <apichai.s@thairth.co.th>
2020-09-23 19:09:23 +0700

    get OK miss save to mongodb

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const {MongoClient} = require('mongodb');
 const dotenv = require('dotenv');
 const axios = require('axios').default;
 const _ = require('lodash');
@@ -6,17 +7,21 @@ const resultENV = dotenv.config();
 if(resultENV.error)
     throw resultENV.error;
 
+const uri = "mongodb+srv://" + process.env.MONGO_USERNAME + ":" + process.env.MONGO_PASSWORD + "@<your-cluster-url>/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
+
 let returnArray = [];
 const getIGHashTagsData = (async (has_next_page=false, end_cursor='') => {
     let nextPage = (has_next_page) ? '&max_id=' + end_cursor : '';
     let defaultURL = encodeURI(process.env.URL_IG_EXPLORE + process.env.TAGS_VAL + '/?__a=1' + nextPage);
     
     setTimeout(function() {
-        console.log(nextPage + '<==========>' + defaultURL);
+        console.log('aaaaaaaaaa===>');
+        console.log(returnArray);
     }, 5000);
 
     axios.get(defaultURL)
-        .then(function(response){
+        .then( response => {
             let hasNextPage = response.data.graphql.hashtag.edge_hashtag_to_media.page_info.has_next_page;
             let endCursor =  response.data.graphql.hashtag.edge_hashtag_to_media.page_info.end_cursor;
             
@@ -26,17 +31,19 @@ const getIGHashTagsData = (async (has_next_page=false, end_cursor='') => {
 
             if(hasNextPage) 
                 getIGHashTagsData(hasNextPage, endCursor);
-            
-            //return returnArray;
         })
-        .catch(function(error) {
+        .catch( error => {
             console.log(error)
             return ('ERROR CONNECTION');
         });
-  
-    console.log('result====>')
-    console.log(returnArray.length);
-    //return ('ERROR');
+    
+    return returnArray;
+});
+
+
+(async ()=>{
+    const entireList = await getIGHashTagsData();
+    console.log(entireList);
 })();
 
 //let responseValue = await getIGHashTagsData();
